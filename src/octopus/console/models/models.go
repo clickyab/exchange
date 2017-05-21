6aa0be2d4fcfedd4312b849e5e9b53d44667d9a6
danicheeta <danicheeta@yahoo.com>
2017-05-21 17:21:31 +0430

    login with post method added

--- src/octopus/console/models/models.go
+++ src/octopus/console/models/models.go
@@ -1,20 +1,26 @@
 package models
 
 import (
+	"fmt"
+
 	"octopus/console/modules/user/aaa"
 	"services/mysql"
 )
 
+//UserTableFull table users
 const UserTableFull = "users"
 
+//Manager models
 type Manager struct {
 	mysql.Manager
 }
 
+//NewOctManager octopus manager
 func NewOctManager() *Manager {
 	return &Manager{}
 }
 
+//Initialize manager
 func (m *Manager) Initialize() {
 	m.AddTableWithName(
 		aaa.User{},
@@ -23,6 +29,22 @@ func (m *Manager) Initialize() {
 		true,
 		"ID",
 	)
+}
+
+// GetUser returns true if the user and pass is ok
+func (m *Manager) GetUser(username, password string) bool {
+	query := fmt.Sprintf(`SELECT * FROM %s where username=? AND password=?`, UserTableFull)
+	user := &aaa.User{}
+	err := m.GetRDbMap().SelectOne(
+		user,
+		query,
+		username,
+		password,
+	)
+	if err != nil || user == nil {
+		return false
+	}
+	return true
 }
 
 func init() {
--- src/octopus/console/modules/user/controllers/user.go
+++ src/octopus/console/modules/user/controllers/user.go
@@ -6,14 +6,16 @@ import (
 	echo2 "gopkg.in/labstack/echo.v3"
 )
 
+//User struct controller
 type User struct {
 	controller.Base
 }
 
-func (User) Routes(r *echo2.Echo, mountPoint string) {
-	r.GET("/login", nil, nil)
-	r.POST("/login", nil, nil)
-	r.GET("logout", nil, nil)
+// Routes adds routers to listener
+func (u User) Routes(r *echo2.Echo, mountPoint string) {
+	r.GET("/login", nil)
+	r.POST("/login", login)
+	r.GET("/logout", nil)
 }
 
 func init() {
