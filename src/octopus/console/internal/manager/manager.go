7b17c50c67316bbfa352a47717315fb9e10b4ee8
danicheeta <danicheeta@yahoo.com>
2017-05-24 12:20:07 +0430

    console added under okhtapoos

---
+++ src/octopus/console/internal/manager/manager.go
@@ -0,0 +1,20 @@
+package manager
+
+import "services/mysql"
+
+type Manager struct {
+	mysql.Manager
+}
+
+// Initialize the model, its the interface, not really need this
+func (m *Manager) Initialize() {
+}
+
+// NewManager return a new manager object
+func NewManager() *Manager {
+	return &Manager{}
+}
+
+func init() {
+	mysql.Register(&Manager{})
+}
---
+++ src/octopus/console/internal/routes/routers.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"context"
+	"net/http"
+)
+
+func loginPost(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func loginGet(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func logout(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
