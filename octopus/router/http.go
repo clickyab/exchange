bb69f7f292760d35cc3277806eb66bf394570bc7
fzerorubigd <fzerorubigd@gmail.com>
2017-07-08 10:41:59 +0430

    use the framework for router
    do not re-invent the wheel
    also fix the migration codegen and remove the metadata

--- octopus/router/http.go
+++ octopus/router/http.go
@@ -2,64 +2,30 @@
 package router
 
 import (
-	"context"
-	"net/http"
-	"time"
-
 	"clickyab.com/exchange/octopus/router/internal/demands"
-	"clickyab.com/exchange/octopus/router/internal/middlewares"
 	"clickyab.com/exchange/octopus/router/internal/restful"
-	"github.com/clickyab/services/config"
-	"github.com/clickyab/services/initializer"
 
-	"github.com/Sirupsen/logrus"
+	"github.com/clickyab/services/framework/router"
 	"github.com/rs/xhandler"
 	"github.com/rs/xmux"
 )
 
-var (
-	listenAddress = config.RegisterString("exchange.router.listen", ":80", "exchange router listen address")
-)
-
+// TODO (NOT IMPORTANT! ITS JUST AN IDEA): Add the annotation so the framework generate all this shit
 type initRouter struct {
 }
 
-func wrap(in xhandler.HandlerFuncC) xhandler.HandlerFuncC {
-	return xhandler.HandlerFuncC(middlewares.Recovery(middlewares.Logger(in)))
-}
-
-func (i initRouter) Initialize(ctx context.Context) {
-	mux := xmux.New()
-	// Restful route
-	mux.POST("/rest/get/:key", wrap(restful.GetAd))
-	mux.GET("/pixel/:demand/:trackID", wrap(restful.TrackPixel))
+func (initRouter) Routes(mux *xmux.Mux, mountPoint string) {
+	mux.POST("/rest/get/:key", xhandler.HandlerFuncC(restful.GetAd))
+	mux.GET("/pixel/:demand/:trackID", xhandler.HandlerFuncC(restful.TrackPixel))
 	// The demand status routes
-	mux.GET("/demands/status/:name", wrap(demands.Status))
-	mux.POST("/demands/status/:name", wrap(demands.Status))
-	mux.DELETE("/demands/status/:name", wrap(demands.Status))
-	mux.HEAD("/demands/status/:name", wrap(demands.Status))
-	mux.PUT("/demands/status/:name", wrap(demands.Status))
-	mux.OPTIONS("/demands/status/:name", wrap(demands.Status))
-
-	srv := &http.Server{Addr: listenAddress.String(), Handler: xhandler.New(ctx, mux)}
-	go func() {
-		if err := srv.ListenAndServe(); err != nil {
-			logrus.Debug(err)
-		}
-	}()
-	logrus.Debugf("Server started on %s", listenAddress.String())
-	go func() {
-		done := ctx.Done()
-		if done != nil {
-			<-done
-			s, _ := context.WithTimeout(context.Background(), 100*time.Millisecond)
-			srv.Shutdown(s)
-			logrus.Debug("Server stopped")
-		}
-	}()
-
+	mux.GET("/demands/status/:name", xhandler.HandlerFuncC(demands.Status))
+	mux.POST("/demands/status/:name", xhandler.HandlerFuncC(demands.Status))
+	mux.DELETE("/demands/status/:name", xhandler.HandlerFuncC(demands.Status))
+	mux.HEAD("/demands/status/:name", xhandler.HandlerFuncC(demands.Status))
+	mux.PUT("/demands/status/:name", xhandler.HandlerFuncC(demands.Status))
+	mux.OPTIONS("/demands/status/:name", xhandler.HandlerFuncC(demands.Status))
 }
 
 func init() {
-	initializer.Register(&initRouter{}, 100)
+	router.Register(&initRouter{})
 }
