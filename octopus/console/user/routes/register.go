353678e9d642c1c2a19ebde6474b6bfdb8c1c724
slifer2015 <dara51php@gmail.com>
2017-07-17 10:01:19 +0430

    comment the registration in exchange
    Fix #2713

--- octopus/console/user/routes/register.go
+++ octopus/console/user/routes/register.go
@@ -1,19 +1,10 @@
 package routes
 
-import (
-	"context"
-	"net/http"
-
-	"errors"
-
-	"clickyab.com/exchange/octopus/console/user/aaa"
-)
-
-type registrationPayload struct {
-	Email    string       `json:"email"`
-	Password string       `json:"password"`
-	UserType aaa.UserType `json:"user_type"`
-}
+//type registrationPayload struct {
+//	Email    string       `json:"email"`
+//	Password string       `json:"password"`
+//	UserType aaa.UserType `json:"user_type"`
+//}
 
 // login user in system
 // @Route {
@@ -24,24 +15,24 @@ type registrationPayload struct {
 //		400 = controller.ErrorResponseSimple
 //		403 = controller.ErrorResponseSimple
 // }
-func (c Controller) register(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-	pl := c.MustGetPayload(ctx).(*registrationPayload)
-	if !pl.UserType.IsValid() {
-		c.BadResponse(w, errors.New("wrong user type"))
-		return
-	}
-	m := aaa.NewAaaManager()
-
-	usr, err := m.RegisterUser(pl.Email, pl.Password, pl.UserType)
-	if err != nil {
-		c.BadResponse(w, errors.New("error registering user"))
-		return
-	}
-	token := getNewToken(usr)
-	c.OKResponse(w, responseLoginOK{
-		ID:       usr.ID,
-		Email:    usr.Email,
-		Token:    token,
-		UserType: usr.UserType,
-	})
-}
+//func (c Controller) register(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+//	pl := c.MustGetPayload(ctx).(*registrationPayload)
+//	if !pl.UserType.IsValid() {
+//		c.BadResponse(w, errors.New("wrong user type"))
+//		return
+//	}
+//	m := aaa.NewAaaManager()
+//
+//	usr, err := m.RegisterUser(pl.Email, pl.Password, pl.UserType)
+//	if err != nil {
+//		c.BadResponse(w, errors.New("error registering user"))
+//		return
+//	}
+//	token := getNewToken(usr)
+//	c.OKResponse(w, responseLoginOK{
+//		ID:       usr.ID,
+//		Email:    usr.Email,
+//		Token:    token,
+//		UserType: usr.UserType,
+//	})
+//}
