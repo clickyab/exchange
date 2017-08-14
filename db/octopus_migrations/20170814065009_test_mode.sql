127d568456589c15aed616deb4a0006337c27731
fzerorubigd <fzerorubigd@gmail.com>
2017-08-14 18:04:24 +0430

    fix the migration

--- db/octopus_migrations/20170814065009_test_mode.sql
+++ db/octopus_migrations/20170814065009_test_mode.sql
@@ -2,12 +2,12 @@
 -- +migrate Up
 -- SQL in section 'Up' is executed when this migration is applied
 
-ALTER TABLE suppliers ADD COLUMN "test_mode" INT DEFAULT 1;
-ALTER TABLE demands ADD COLUMN "test_mode" INT DEFAULT 1;
+ALTER TABLE suppliers ADD COLUMN test_mode INT DEFAULT 1;
+ALTER TABLE demands ADD COLUMN test_mode INT DEFAULT 1;
 
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
 
-ALTER TABLE suppliers DROP COLUMN "test_mode";
-ALTER TABLE demands DROP COLUMN "test_mode";
+ALTER TABLE suppliers DROP COLUMN test_mode;
+ALTER TABLE demands DROP COLUMN test_mode;
 
