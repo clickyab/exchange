94cb0d8041487d7fdee8da0c427f7b8fee2a6155
daniel <danicheeta@yaoo.com>
2017-09-24 18:10:38 +0330

    demo demand fake response
    Fix #3623

--- demos/routers.go
+++ demos/routers.go
@@ -1,7 +1,12 @@
 package demos
 
 import (
+	"context"
+	"net/http"
+
+	"github.com/clickyab/services/framework"
 	"github.com/clickyab/services/framework/router"
+	"github.com/rs/xhandler"
 	"github.com/rs/xmux"
 )
 
@@ -9,6 +14,18 @@ type initRouter struct {
 }
 
 func (initRouter) Routes(mux *xmux.Mux, mountPoint string) {
+	mux.POST("/", xhandler.HandlerFuncC(getFakeResponse))
+}
+
+func getFakeResponse(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	request, err := bidRequestParser(r)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	response := bidFakeResponse(request)
+	framework.JSON(w, http.StatusOK, response)
 }
 
 func init() {
