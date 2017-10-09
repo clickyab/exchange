bd1dc5da850110aa5b4b5a4efa7c80b700a23124
daniel <danicheeta@yaoo.com>
2017-10-09 14:27:59 +0330

    impression object
    Fix #3773

--- octopus/suppliers/internal/openrtb/impression.go
+++ octopus/suppliers/internal/openrtb/impression.go
@@ -1,3 +1,41 @@
 package openrtb
 
+import (
+	"clickyab.com/exchange/octopus/exchange"
+	"github.com/bsm/openrtb"
+	"github.com/sirupsen/logrus"
+)
 
+type impression struct {
+	openrtb.Impression
+	attributes map[string]interface{}
+}
+
+func (imp impression) FID() string {
+	return imp.ID
+}
+
+func (imp impression) FBidFloor() float64 {
+	return imp.BidFloor
+}
+
+func (imp impression) Attributes() map[string]interface{} {
+	return imp.attributes
+}
+
+func (imp impression) Type() exchange.ImpressionType {
+	if imp.Banner != nil {
+		return exchange.AdTypeBanner
+	} else if imp.Native != nil {
+		return exchange.AdTypeNative
+	} else if imp.Audio != nil {
+		logrus.Panicln("audio ad not supported")
+	}
+
+	return exchange.AdTypeVideo
+}
+
+func (imp impression) FSecure() bool {
+	return int(imp.Secure) == 1
+
+}
